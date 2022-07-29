9f4c535665e7214856b87ed0ecbf803bb3bed3fd
Bonim OUK <bonimouk@gmail.com>
2022-07-28 17:28:08 -0700

    answer

--- app.js
+++ app.js
@@ -7,13 +7,38 @@ console.log(
 console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
+let a = 20;
+let b = 4;
+
+let c = a + b;
+let d = a - b;
+let e = a * b;
+let f = a / b;
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
+1111;
+11eleven;
+12;
+Frodo11;
+true11;
+FrodoBaggins;
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+true;
+false;
+true;
+false;
+true;
+true;
+false;
+false;
+true;
+
